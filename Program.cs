cfa105b78d5f66fe02afefe6c5ac857b3d76b059
Nuriddin321 <donkoko1996@gmail.com>
2022-07-17 14:36:45 +0500

    botBackgroundService added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
+using surahSender.Services;
+using Telegram.Bot;
+
 var builder = WebApplication.CreateBuilder(args);
- 
+
+var token = builder.Configuration.GetValue("BotToken", string.Empty);
+
+builder.Services.AddSingleton(p => new TelegramBotClient(token));
+builder.Services.AddHostedService<BotBackgroundService>();
+
 var app = builder.Build();
- 
+
 app.Run();
