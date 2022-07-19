bd00b84dfd89594ba24352fac77430c00a6fb9b6
Turdialiyev <turdialiyevgolibjon0226@gmail.com>
2022-07-20 00:07:58 +0500

    saved information to Data

--- Services/Handlers/BotUpdateHandler.ChannelPost.cs
+++ Services/Handlers/BotUpdateHandler.ChannelPost.cs
@@ -7,12 +7,36 @@ namespace SurahSender.Services;
 
 public partial class BotUpdateHandler
 {
+    private QuranService _quranService;
+
     private async Task HandlerChannelPostAsync(ITelegramBotClient botClient, Message? channelPost, CancellationToken cancellationToken)
     {
         ArgumentNullException.ThrowIfNull(channelPost);
 
-        var id = channelPost.MessageId;
-        _logger.LogInformation("Id Of message {id}", id);
+        using var scope = _scopeFactory.CreateScope();
+        
+        _quranService = scope.ServiceProvider.GetRequiredService<QuranService>();
+        
+        var idOfMessage = channelPost.MessageId;
+        var name = channelPost.Caption;
+        var size = 1;
 
+        _logger.LogInformation("Id Of message {id}", idOfMessage);
+
+        var result = await _quranService.AddDataAsync(new Entities.Quran()
+        {
+            IdOfMessage = idOfMessage,
+            Name = name,
+            Size = size
+        });
+
+        if (result.IsSuccess)
+        {
+            _logger.LogInformation($"New Quran Video successfully added: {idOfMessage}, Name: {name}");
+        }
+        else
+        {
+            _logger.LogInformation($"Quran video not added: {idOfMessage}, Error: {result.ErrorMessage}");
+        }
     }
 }
